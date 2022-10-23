f1bada398e662d79e71661ec83b0ba4299065c68
Dhruv Joshi <joshidhruv997@gmail.com>
2022-10-23 23:00:01 +0530

    configure mysql persistence data source

---
+++ isengard-server/src/main/webapp/WEB-INF/classes/db/mysql/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES('administrator', 'password', true);
+INSERT INTO authorities VALUES('administrator', 'ROLE_ADMIN');
+INSERT INTO authorities VALUES('administrator', 'ROLE_USER');
+INSERT INTO user_info(sub, preferred_name, name, email, email_verified) VALUES('3789.ABC3SE', 'administrator', 'Admin User', 'admin@isengard.io', true);
